ddc211a628ab796216ea13a5096a09b825be392f
annaumnova <75691306+annaumnova@users.noreply.github.com>
2021-12-17 21:16:41 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+//input_string is an example or expression that we need to process
+//stack is the store place
+//output_string is the output data
+#include <iostream>
+#include <string.h>
+#include <stdexcept>
+#include "PPx Calculator.h"//Polish Postfix Calculator
+
+void instruction()
+{
+	std::cout << "Operations can be used:\n" <<
+		"* arithmetics: +, -, *, / , ^\n" <<
+		"* trigonometric : sin, cos, tg, ctg, Arch\n" <<
+		"* logarithmic : ln, log, sqrt\n"<<
+		"It works with numbers, not with letters"
+		<< std::endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "English");
+	instruction();
+	std::cout << "Please, enter your expression: " << std::endl;
+	postfix_calculating();//polish postfix calculator
+	return 0;
+}
